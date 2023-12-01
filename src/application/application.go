2ba2727b2833602bbd1e17f0dac0d06633d44e95
Matt Tey <33141176+mystardustcaptain@users.noreply.github.com>
2023-12-01 08:14:31 +0000

    progress 2_6

---
+++ src/application/application.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"example.com/backend"
+)
+
+func main() {
+	backend.BEFunc()
+	backend.Connect()
+
+}
---
+++ src/backend/backend.go
@@ -0,0 +1,67 @@
+package backend
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type App struct {
+	Port 			string
+	DB 				*sql.DB
+	DBType		string
+	DBPath		string
+}
+
+func (a *App) Initialize() {
+	a.DB, err := sql.Open("sqlite3", "./practiceit.db")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func (a *App) Run() {
+	http.HandleFunc("/", helloWorld)
+	fmt.Println("Server started on port ", a.Port)
+	log.Fatal(http.ListenAndServe(a.Port, nil))
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World\n")
+}
+
+
+func (a *App) GetData() {
+	rows, err := a.DB.Query("SELECT id, name, inventory, price FROM products")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var p Product
+
+		rows.Scan(&p.id, &p.name, &p.inventory, &p.price)
+		fmt.Println("Product: ", p.id, " ", p.name, " ", p.inventory, " ", p.price)
+	}
+}
+
+type Product struct {
+	id        int
+	name      string
+	inventory int
+	price     int
+}
+
+func BEFunc() {
+	fmt.Println("Hello World!")
+}
+
+func Connect() {
+	dbc := DBConnection{dbType: "sqlite3", dbPath: "../../practiceit.db"}
+	dbc.Connect()
+	dbc.GetData()
+}
